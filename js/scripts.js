1972ea0122b28216d7a2e2a8a05907d036239a19
shashank seth <shashankseth3@gmail.com>
2020-12-23 21:18:37 +0530

    header updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+function openCity(cityName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("main-categories");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(cityName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+
+}
