6234bcf709e85457bec664429799a5004b23eaf9
shashank seth <shashankseth3@gmail.com>
2020-12-28 01:43:14 +0530

    updated css

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
-
-// Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn-profile')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content-profile");
+    if (!event.target.matches("dropbtn")) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
@@ -16,4 +12,14 @@ window.onclick = function(event) {
             }
         }
     }
+}
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
 }
