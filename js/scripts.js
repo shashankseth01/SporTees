dc4b89018f51c7ec0f709a25efcb7598eddd327c
shashank seth <shashankseth3@gmail.com>
2020-12-29 20:07:51 +0530

    updated css

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,3 @@ window.onclick = function(event) {
         }
     }
 }
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-50px";
-    }
-    prevScrollpos = currentScrollPos;
-}
