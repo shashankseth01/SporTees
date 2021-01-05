216a3b01d9dbdb71df1069fc016c5e4bfa8e4159
shashank seth <shashankseth3@gmail.com>
2021-01-06 04:07:10 +0530

    updated css

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,7 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+var modal = document.getElementById('id01');
+
 window.onclick = function(event) {
-    if (!event.target.matches("dropbtn")) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
